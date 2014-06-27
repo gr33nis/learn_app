195f7b7797e6c73bc1eec255941c424c10d6f0b4
Ian Green <gr33nis@hotmail.com>
2014-06-26 22:33:45 -0400

    finished user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name "Ian Green"
+        email "gr33nis@hotmail.com"
+        password "foobar"
+        password_confirmation "foobar"
+    end
+end
